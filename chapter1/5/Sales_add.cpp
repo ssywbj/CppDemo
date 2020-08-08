34a3ef6f461ef9def7ab2032d0f98398218b0b82
ssywbj <1271829012@qq.com>
2020-08-09 01:46:37 +0800

    完善book add程序

--- chapter1/5/Sales_add.cpp
+++ chapter1/5/Sales_add.cpp
@@ -6,11 +6,15 @@ using namespace std;
 
 int main()
 {
-    //两本相同编号(ISBN)的书相加，即对象相加：对象的成员对应相加的结果
+    //两本相同编号(ISBN)的书相加，即对象相加：对象的成员对应相加，结果是一个新对象
     Sales_item item1, item2;
     cin >> item1 >> item2;
-    cout << "items sum:" << item1 + item2 << endl;
-
-    return 0;
+    if(item1.isbn() == item2.isbn()){
+        cout << "items sum:" << item1 + item2 << endl;
+        return 0;
+    } else {
+        cout << "Data must refer to same ISBN" << endl;
+        return -1;
+    }
 }
 
