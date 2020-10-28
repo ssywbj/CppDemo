2e9ab3995ece46dc5b26fba718ab4bc39b904c21
ssywbj <weibj@zhipu-inc.com>
2020-10-28 23:38:38 +0800

    修改错误字

--- chapter2/10.2_struct.cpp
+++ chapter2/10.2_struct.cpp
@@ -5,7 +5,7 @@ struct Sales_data {
 	string bookNo;
 	unsigned units_sold = 0;
 	double revenue = 0;
-};//自定义数据结构方式一：变量名和类休分开。推荐这种方式
+};//自定义数据结构方式一：变量名和类体分开。推荐这种方式
 
 struct Sales {
 	string bookNo;
