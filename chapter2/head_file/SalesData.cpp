db9ca9ade697e319b76b9a904853fc01cd85fbcb
ssywbj <weibj@zhipu-inc.com>
2020-11-04 00:56:53 +0800

    1.使用命名空间注意事项；2.string变量声明

--- chapter2/head_file/SalesData.cpp
+++ chapter2/head_file/SalesData.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "SalesData.h"
 
+//使用命名空间的另一种方法
+//using namespace::name;
+//每个using声明引入命名空间中的一个成员，这是使用命名空间最安全的方法。好
+//处有两个：一是可以减少注入到命名空间中的名字数量，二是using声明引起的二
+//义性问题在声明处就能发现，无须等到使用名字的地方，这对检测并修改错误有
+//很大的益处。
+using std::cin;
+using std::cout;
+using std::endl;
+
 int main()
 {
 	SalesData data1, data2;
--- chapter2/head_file/SalesData.h
+++ chapter2/head_file/SalesData.h
@@ -1,12 +1,15 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 
-//#include <string>
-using namespace std;
+#include <string>
+//using namespace std;//一般来说位于头文件的代码不应该使用using声明。因为头
+//文件的内容会拷贝到所有引用它的头文件中，如果头文件里有某个using声明，那么
+//每个使用该头文件的文件就都会有这个声明。对于某些程序来说，由于不经意间包
+//含了一些名字，反而可能产生始料未及的名字冲突。
 
 struct SalesData {
-	//std::string bookNo;
-	string bookNo;
+	std::string bookNo;
+	//string bookNo;
 	unsigned units_sold;
 	double revenve;
 };
