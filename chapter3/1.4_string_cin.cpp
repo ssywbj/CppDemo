8486b203df9c7b2cc1e37b30f3e3c44199d06c3c
weibj <weibj@zhipu-inc.com>
2020-11-05 15:05:13 +0800

    ==、<、+、+=

--- chapter3/1.4_string_cin.cpp
+++ chapter3/1.4_string_cin.cpp
@@ -9,13 +9,31 @@ using std::string;
 int main()
 {
     //读取数量未知的sring
-    unsigned index = 0;
-    string word;
+    unsigned count = 0;
+    string word, append;
     while (cin >> word) {//空白(即空格符、换行符等)并不会读入，不进行循环体
-        index++;
-        cout << "index: " << index << ", word: " << word << endl;
+        count++;
+        append += word;//字符串累加
+        cout << "count: " << count << ", word: " << word 
+        << ", append: " << append << endl;
     }
-    cout << "total: " << index << endl;
+    cout << "total: " << count << endl;
+
+    //为了与C兼容及某些历史原因，C++语言中的字符串字面值并不是标准
+    //库类型string的对象。切记，字符串字面值与string是不同的类型。
+
+    //当把string对象和字符字面值及字符串字面值混在一条语句使用的
+    //时候，必须确保每个加法运算符(+)的两侧的运算对象至少有一个
+    //是string。
+    string add = word + " Add";//正确
+    //string s2 = "Hello " + "World";//错误："+"号两侧都是字符串字面值
+    string s3 = word + ", Hello" + " World";//正确，等价于：
+    //string temp = word +", Hello";//正确
+    //string s3 = temp + " World";//正确
+
+    //string s4 = "Hello " + "World, " + word;//错误，等价于：
+    //string temp = "Hello " + "World, ";//错误：不能把字面值直接相加
+    cout << "add: " << add << ", s3: " << s3 << endl;
 
     return 0;
 }
--- chapter3/1.5_string_cin.cpp
+++ chapter3/1.5_string_cin.cpp
@@ -7,25 +7,28 @@ using std::endl;
 using std::string;
 
 int main()
-{
-//getline(cin, string)函数读取一整行，包括空白符也一块读取。
+{	
+    //getline(cin, string)函数读取一整行，包括空白符也一块读取。
 
-//size()函数的返回类型其实是string::size_type，它是string类中定义的
-//一种配套类型之一。尽管我们不太清楚string::size_type类型的细节，但是
-//有一点是肯定的：它是一个无符号类型的值而且能足够存放下任何string
-//对象的大小。由于size()函数返回的一个无符号整数，因此在表达式中就不
-//要使用有符号整数了，以避免混合使用无符号数和有符号数带来的问题。
-    unsigned index = 0;
+    //size()函数的返回类型其实是string::size_type，它是string类中定义的
+    //一种配套类型之一。尽管我们不太清楚string::size_type类型的细节，但是
+    //有一点是肯定的：它是一个无符号类型的值而且能足够存放下任何string
+    //对象的大小。由于size()函数返回的一个无符号整数，因此在表达式中就不
+    //要使用有符号整数了，以避免混合使用无符号数和有符号数带来的问题。
+
+    unsigned count = 0;
     string line;
     while (getline(cin, line)) {
         if(line.empty()){
             cout << "line string is null" << endl;
         } else {
-            index++;
-            cout << "line: " << line << ", len: " << line.size() << endl;
+            count++;
+            const unsigned len = line.size(), index = len - 1;
+            cout << "line: " << line << ", len: " << len
+                << ", line[" << index << "]: " << line[index] <<endl;
         }
     }
-    cout << "total: " << index << endl;
+    cout << "total: " << count << endl;
 
     return 0;
 }
