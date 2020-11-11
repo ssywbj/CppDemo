36b75311e9ed9d1e6ba2cf608f0037533e271c01
weibj <weibj@zhipu-inc.com>
2020-11-11 15:04:17 +0800

    iterator operation

--- chapter3/3.1_iterator.cpp
+++ chapter3/3.1_iterator.cpp
@@ -5,15 +5,42 @@ using std::endl;
 #include <string>
 using std::string;
 
+#include <vector>
+using std::vector;
+
 int main()
 {
     string str("some thing");
     if(str.begin() != str.end()){
-        auto ch = str.begin();
-        cout << "ch: " << *ch << endl;
+        auto begin = str.begin();
+        cout << "begin: " << *begin << endl;
+        //cout << "begin: " << begin << endl;//错误，需要加*
+        *begin = toupper(*begin);
+
+        string::iterator end = str.end();
+        cout << "end: " << *end << endl;
+        cout << "previous end: " << *(--end) << endl;
+
+        string::const_iterator iter = str.begin();
+        cout << "iter: " << *iter << endl;
+        //*iter = tolower(*iter);
+    }
+    cout << str << endl;
+
+    //依次处理字符直到处理完全部字符或遇到空白符
+    for(auto ch = str.begin();ch != str.end() && !isspace(*ch);ch++){
         *ch = toupper(*ch);
     }
     cout << str << endl;
+
+    string v;
+    bool b = v.empty();
+    cout << "b: " << b << ", bool: " << v.empty() << "---" << (v.begin() == v.end()) << endl;
+
+    vector<string> vStr(10, "H");
+    vector<string>::iterator it = vStr.begin();
+    cout << "judge string object is empty or is not: " << (*it).empty() << endl;
+    cout << "judge string object is empty or is not: " << it->empty() << endl;
 
     return 0;
 }
